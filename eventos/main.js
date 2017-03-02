f57f26317a0f436f3398fd6031b76a578f79be69
Victor Suarez <jr._5oct1998@hotmail.com>
2017-03-02 13:01:29 -0600

    Ejemplos de eventos

---
+++ eventos/main.js
@@ -0,0 +1,17 @@
+function saludar()
+{
+    alert('Hola eventos')
+}
+
+function saludar2()
+{
+    document.write('Hola document')
+}
+
+var boton_saluda = document.getElementById('btn-saluda')
+boton_saluda.onclick =saludar
+boton_saluda.onclick = saludar2
+
+var boton_saluda2 = document.getElementById('btn-saluda2')
+boton_saluda2.addEventListener('click', saludar)
+boton_saluda2.addEventListener('click', saludar2)
